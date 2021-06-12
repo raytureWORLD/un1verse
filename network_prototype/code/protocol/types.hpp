0a33f0952ab5f93259277cbfa349457d7a8b3f56
_ <_>
2021-06-12 18:37:13 +0200

    Added stuff

---
+++ network_prototype/code/protocol/types.hpp
@@ -0,0 +1,13 @@
+#ifndef INCLUDED_PROTOCOL_TYPES_HPP
+#define INCLUDED_PROTOCOL_TYPES_HPP 
+
+#include<cstdint>
+
+namespace Network {
+    namespace Protocol {
+        typedef uint32_t Packet_length;
+        typedef uint32_t Packet_id;
+    }
+}
+
+#endif
