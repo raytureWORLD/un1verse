1c1abd3f1dbc761f270a1070a43008b322072337
_ <_>
2021-06-13 16:42:46 +0200

    Work in progress

--- network_prototype/code/main/main.cpp
+++ network_prototype/code/main/main.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include"io/console.hpp"
-
+#include"server/connection_manager.hpp"
 
 int main(int _argc, char** _argv) {
     using namespace Network;
@@ -8,5 +8,13 @@ int main(int _argc, char** _argv) {
 
 
     Console::write_line("Running");
+    for(;;) {
+        Server::Connection_manager cm(2345);
+
+        int x;
+        std::cin >> x;
+    }
+    Console::write_line("Done");
+
 
 }
