65e78e13f580fb32efb9c8d9e6cd6055708282ce
_ <_>
2021-06-14 14:55:40 +0200

    Changes

--- network_prototype/code/server/connection_manager.cpp
+++ network_prototype/code/server/connection_manager.cpp
@@ -3,6 +3,7 @@
 #include<functional>
 
 Network::Server::Connection_manager::Connection_manager(uint16_t _port_number, unsigned _packet_queue_capacity):
+    packet_queue_capacity(_packet_queue_capacity),
     io_context_thread(
         std::bind_front(
             &Connection_manager::io_context_thread_function,
@@ -17,83 +18,25 @@ Network::Server::Connection_manager::Connection_manager(uint16_t _port_number, u
             _port_number
         )
     ),
-    next_connection_id(0),
-    packet_queue_capacity(_packet_queue_capacity)
+    next_connection_id(0)
 {
-    acceptor.async_accept(std::bind_front(&Connection_manager::async_accept_callback, this));
-}
-
-
-void Network::Server::Connection_manager::tick() {
-    /* Copy to avoid holding the mutex for too long */
-    std::vector<boost::asio::ip::tcp::socket> accepted_sockets_copy;
-    
-    {
-        std::scoped_lock lock(accepted_sockets_mx);
-        accepted_sockets_copy = std::move(accepted_sockets);
-        accepted_sockets.clear();
-    }
-
-    for(auto& socket : accepted_sockets_copy) {
-        auto [iterator, success] = connections.emplace(
-            std::piecewise_construct,
-            std::forward_as_tuple(next_connection_id),
-            std::forward_as_tuple(std::move(socket), next_connection_id) 
-        );
-
-        auto& connection = iterator->second;
-
-        /* Initiate receiving packets */
-        boost::asio::async_read(
-            connection.socket,
-            boost::asio::buffer(
-                reinterpret_cast<std::byte*>(&connection.next_inbound_packet_length),
-                sizeof(connection.next_inbound_packet_length)
-            ),
-            std::bind_front(
-                &Connection_manager::async_read_callback,
-                this,
-                connection.id,
-                Async_read_operation_type::length
-            )
-        );
-
-        Events::Connection_established event(
-            connection.id,
-            connection.socket.local_endpoint().address(),
-            connection.socket.local_endpoint().port(),
-            connection.socket.remote_endpoint().address(),
-            connection.socket.remote_endpoint().port()
-        );
-
-        post_event(event);
-        
-        ++next_connection_id;
-    }
-
-
-    std::vector<std::unique_ptr<Protocol::Inbound_packet>> inbound_packets_queue_copy;
-    {
-        std::scoped_lock lock(inbound_packets_queue_mx);
-        inbound_packets_queue_copy = std::move(inbound_packets_queue);
-        inbound_packets_queue.clear();
-    }
-
-    for(auto const& packet : inbound_packets_queue_copy) {
-        Events::Packet_received event(
-            0, //TODO: Change this
-            packet.get()
-        );
-
-        post_event(event);
-    }
+    async_accept();
 }
 
 Network::Server::Connection_manager::~Connection_manager() {
     io_context.stop();
 }
 
-void Network::Server::Connection_manager::io_context_thread_function([[maybe_unused]] std::stop_token _stop_token) {
+
+void Network::Server::Connection_manager::tick() {
+    process_accepted_sockets();
+    process_killed_connections();
+    process_received_packets();
+}
+
+void Network::Server::Connection_manager::io_context_thread_function(std::stop_token _stop_token) {
+    (void)_stop_token;
+
     auto work = boost::asio::require(io_context.get_executor(), boost::asio::execution::outstanding_work.tracked);
 
     /* Exceptions thrown from handlers will be caught here */
@@ -108,94 +51,283 @@ void Network::Server::Connection_manager::io_context_thread_function([[maybe_unu
     }
 }
 
+void Network::Server::Connection_manager::async_accept() {
+    acceptor.async_accept(std::bind_front(&Connection_manager::async_accept_callback, this));
+}
+
 void Network::Server::Connection_manager::async_accept_callback(
     boost::system::error_code const& _error,
     boost::asio::ip::tcp::socket _peer_socket
 ) {
     if(!_error) {
-        {
-            std::scoped_lock lock(accepted_sockets_mx);
-            accepted_sockets.emplace_back(std::move(_peer_socket));
-        }
+        std::scoped_lock lock(accepted_sockets_mx);
+        accepted_sockets.emplace_back(std::move(_peer_socket));
     } else {
-        throw std::runtime_error(_error.message());
+        throw std::runtime_error(
+            Text::concatenate(
+                "Network::Server::Connection_manager::async_accept_callback() error: ",
+                _error.message()
+            )
+        );
     }
 
     acceptor.async_accept(std::bind_front(&Connection_manager::async_accept_callback, this));
 }
 
 
-void Network::Server::Connection_manager::async_read_callback(
-    Connection::Id _connection_id,
-    Async_read_operation_type _operation_type,
-    boost::system::error_code const& _error,
-    [[maybe_unused]] std::size_t _bytes_transferred
-) {
-    if(!_error) {
-        auto connection_iterator = connections.find(_connection_id);
-        if(connection_iterator == std::end(connections)) {
-            throw std::logic_error("Async_read: no connection with the specified id");
-        }
+void Network::Server::Connection_manager::process_accepted_sockets() {
+    /* Copy to avoid holding the mutex for too long */
+    std::vector<boost::asio::ip::tcp::socket> accepted_sockets_copy;
+    {
+        std::scoped_lock lock(accepted_sockets_mx);
+        accepted_sockets_copy = std::move(accepted_sockets);
+        accepted_sockets.clear();
+    }
 
-        auto& connection = connection_iterator->second;
+    for(auto& socket : accepted_sockets_copy) {
+        auto [iterator, success] = connections.emplace(
+            std::piecewise_construct,
+            std::forward_as_tuple(next_connection_id),
+            std::forward_as_tuple(
+                std::make_shared<Connection>(
+                    next_connection_id,
+                    std::move(socket)
+                )
+            )
+        );
+        ++next_connection_id;
 
-        switch(_operation_type) {
-            case Async_read_operation_type::length: {
-                connection.next_inbound_packet_data = std::make_unique_for_overwrite<std::byte[]>(
-                    connection.next_inbound_packet_length
-                );
+        /* Initiate receiving packets */
+        async_read(iterator->second, Async_read_operation_type::length);
 
-                std::size_t offset = Serialisation::Binary::write(
-                    connection.next_inbound_packet_length,
-                    connection.next_inbound_packet_data.get(),
-                    connection.next_inbound_packet_data.get() + connection.next_inbound_packet_length
-                );
+        Events::Connection_established event(
+            iterator->second->id,
+            iterator->second->socket.local_endpoint().address(),
+            iterator->second->socket.local_endpoint().port(),
+            iterator->second->socket.remote_endpoint().address(),
+            iterator->second->socket.remote_endpoint().port()
+        );
 
-                boost::asio::async_read(
-                    connection.socket,
-                    boost::asio::buffer(
-                        connection.next_inbound_packet_data.get() + offset,
-                        connection.next_inbound_packet_length - offset
-                    ),
-                    std::bind_front(
-                        &Connection_manager::async_read_callback,
-                        this,
-                        _connection_id,
-                        Async_read_operation_type::rest
-                    )
-                );
-                break;
-            }
-
-            case Async_read_operation_type::rest: {
-                boost::asio::async_read(
-                    connection.socket,
-                    boost::asio::buffer(
-                        reinterpret_cast<std::byte*>(&connection.next_inbound_packet_length),
-                        sizeof(connection.next_inbound_packet_length)
-                    ),
-                    std::bind_front(
-                        &Connection_manager::async_read_callback,
-                        this,
-                        _connection_id,
-                        Async_read_operation_type::length
-                    )
-                );
-
-                {
-                    std::scoped_lock lock(inbound_packets_queue_mx);
-                    inbound_packets_queue.emplace_back(
-                        std::make_unique<Protocol::Inbound_packet>(
-                            std::move(connection.next_inbound_packet_data),
-                            connection.next_inbound_packet_length
-                        )
-                    );
-                }
-
-                break;
-            }
-        }
-    } else {
-        throw std::runtime_error(_error.message());
+        post_event(event);
     }
 }
+
+void Network::Server::Connection_manager::process_killed_connections() {
+    std::vector<Events::Connection_killed> connection_killed_events_copy;
+    {
+        std::scoped_lock lock(connection_killed_events_mx);
+        connection_killed_events_copy = std::move(connection_killed_events);
+        connection_killed_events.clear();
+    }
+
+    for(auto& event : connection_killed_events_copy) {
+        connections.erase(event.id);
+        post_event(event);
+    }
+}
+
+void Network::Server::Connection_manager::process_received_packets() {
+    std::vector<Events::Packet_received> packet_received_events_copy;
+    {
+        std::scoped_lock lock(packet_received_events_mx);
+        packet_received_events_copy = std::move(packet_received_events);
+        packet_received_events.clear();
+    }
+
+    for(auto& event : packet_received_events_copy) {
+        post_event(event);
+    }
+}
+
+void Network::Server::Connection_manager::async_read(
+    std::shared_ptr<Connection> const& _connection, 
+    Async_read_operation_type _operation
+) {
+    switch(_operation){
+        case Async_read_operation_type::length: {
+            boost::asio::async_read(
+                _connection->socket,
+                boost::asio::buffer(
+                    reinterpret_cast<std::byte*>(&_connection->next_inbound_packet_length),
+                    sizeof(_connection->next_inbound_packet_length)
+                ),
+                std::bind_front(
+                    &Connection_manager::async_read_callback,
+                    this,
+                    _connection,
+                    Async_read_operation_type::length
+                )
+            );
+            break;
+        }
+
+        case Async_read_operation_type::rest: {
+            boost::asio::async_read(
+                _connection->socket,
+                boost::asio::buffer(
+                    _connection->next_inbound_packet_data.get() + sizeof(_connection->next_inbound_packet_length),
+                    _connection->next_inbound_packet_length - sizeof(_connection->next_inbound_packet_length)
+                ),
+                std::bind_front(
+                    &Connection_manager::async_read_callback,
+                    this,
+                    _connection,
+                    Async_read_operation_type::rest
+                )
+            );
+            break;
+        }
+    }
+}
+
+
+void Network::Server::Connection_manager::async_read_callback(
+    std::shared_ptr<Connection> _connection,
+    Async_read_operation_type _operation, /* the operation that finished with this callback */
+    boost::system::error_code const& _error,
+    std::size_t _bytes_transferred
+) {
+    (void)_bytes_transferred;
+
+    if(!_connection->is_dead) {
+        if(!_error) {
+            switch(_operation) {
+                case Async_read_operation_type::length: {
+                    _connection->next_inbound_packet_data = std::make_unique_for_overwrite<std::byte[]>(
+                        _connection->next_inbound_packet_length
+                    );
+
+                    async_read(_connection, Async_read_operation_type::rest);
+
+                    break;
+                }
+
+                case Async_read_operation_type::rest: {
+                    {
+                        std::scoped_lock lock(packet_received_events_mx);
+                        packet_received_events.emplace_back(
+                            _connection->id,
+                            std::make_unique<Protocol::Inbound_packet const>(
+                                std::move(_connection->next_inbound_packet_data),
+                                _connection->next_inbound_packet_length
+                            )
+                        );
+                    }
+
+                    async_read(_connection, Async_read_operation_type::length);
+
+                    break;
+                }
+            }
+        } else {
+            _connection->is_dead = true;
+            _connection->socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+            _connection->socket.close(); /* This cancels all outstanding async operations */
+
+            {
+                std::scoped_lock lock(connection_killed_events_mx);
+                connection_killed_events.emplace_back(
+                    _connection->id,
+                    _error.message()
+                );
+            }
+        }
+    }
+}
+
+// void Network::Server::Connection_manager::tick() {
+//     std::vector<std::unique_ptr<Protocol::Inbound_packet>> inbound_packets_queue_copy;
+//     {
+//         std::scoped_lock lock(inbound_packets_queue_mx);
+//         inbound_packets_queue_copy = std::move(inbound_packets_queue);
+//         inbound_packets_queue.clear();
+//     }
+
+//     for(auto const& packet : inbound_packets_queue_copy) {
+//         Events::Packet_received event(
+//             0, //TODO: Change this
+//             packet.get()
+//         );
+
+//         post_event(event);
+//     }
+// }
+
+
+
+
+
+// void Network::Server::Connection_manager::async_read_callback(
+//     Connection::Id _connection_id,
+//     Async_read_operation_type _operation_type,
+//     boost::system::error_code const& _error,
+//     [[maybe_unused]] std::size_t _bytes_transferred
+// ) {
+//     if(!_error) {
+//         auto connection_iterator = connections.find(_connection_id);
+//         if(connection_iterator == std::end(connections)) {
+//             throw std::logic_error("Async_read: no connection with the specified id");
+//         }
+
+//         auto& connection = connection_iterator->second;
+
+//         switch(_operation_type) {
+//             case Async_read_operation_type::length: {
+//                 connection.next_inbound_packet_data = std::make_unique_for_overwrite<std::byte[]>(
+//                     connection.next_inbound_packet_length
+//                 );
+
+//                 std::size_t offset = Serialisation::Binary::write(
+//                     connection.next_inbound_packet_length,
+//                     connection.next_inbound_packet_data.get(),
+//                     connection.next_inbound_packet_data.get() + connection.next_inbound_packet_length
+//                 );
+
+//                 boost::asio::async_read(
+//                     connection.socket,
+//                     boost::asio::buffer(
+//                         connection.next_inbound_packet_data.get() + offset,
+//                         connection.next_inbound_packet_length - offset
+//                     ),
+//                     std::bind_front(
+//                         &Connection_manager::async_read_callback,
+//                         this,
+//                         _connection_id,
+//                         Async_read_operation_type::rest
+//                     )
+//                 );
+//                 break;
+//             }
+
+//             case Async_read_operation_type::rest: {
+//                 boost::asio::async_read(
+//                     connection.socket,
+//                     boost::asio::buffer(
+//                         reinterpret_cast<std::byte*>(&connection.next_inbound_packet_length),
+//                         sizeof(connection.next_inbound_packet_length)
+//                     ),
+//                     std::bind_front(
+//                         &Connection_manager::async_read_callback,
+//                         this,
+//                         _connection_id,
+//                         Async_read_operation_type::length
+//                     )
+//                 );
+
+//                 {
+//                     std::scoped_lock lock(inbound_packets_queue_mx);
+//                     inbound_packets_queue.emplace_back(
+//                         std::make_unique<Protocol::Inbound_packet>(
+//                             std::move(connection.next_inbound_packet_data),
+//                             connection.next_inbound_packet_length
+//                         )
+//                     );
+//                 }
+
+//                 break;
+//             }
+//         }
+//     } else {
+//         throw std::runtime_error(_error.message());
+//     }
+// }
--- network_prototype/code/server/connection_manager.hpp
+++ network_prototype/code/server/connection_manager.hpp
@@ -9,6 +9,7 @@
 #include<vector>
 #include<mutex>
 #include<unordered_map>
+#include<string>
 #include"protocol/inbound_packet.hpp"
 #include"protocol/outbound_packet.hpp"
 #include"events/sync_event_emitter.hpp"
@@ -18,12 +19,13 @@ namespace Network {
         struct Connection {
             typedef unsigned Id;
 
-            boost::asio::ip::tcp::socket socket;
             Id const id;
+            boost::asio::ip::tcp::socket socket;
+            bool is_dead = false;
+
             Protocol::Packet_length next_inbound_packet_length;
             /* Full data, including all fields of the header - the length too */
             std::unique_ptr<std::byte[]> next_inbound_packet_data;
-
         };
 
         /* TODO: This namespace should be directly inside Network */
@@ -36,9 +38,14 @@ namespace Network {
                 boost::asio::ip::port_type const remote_endpoint_port;
             };
 
+            struct Connection_killed {
+                Connection::Id const id;
+                std::string const reason;
+            };
+
             struct Packet_received {
                 Connection::Id const from;
-                Protocol::Inbound_packet const* const packet;
+                std::unique_ptr<Protocol::Inbound_packet const> packet;
             };
         };
 
@@ -46,6 +53,7 @@ namespace Network {
         class Connection_manager: 
             public Sync_event_emitter<
                 Events::Connection_established,
+                Events::Connection_killed,
                 Events::Packet_received
             > 
         {
@@ -62,39 +70,44 @@ namespace Network {
             Connection_manager& operator=(Connection_manager&&) = delete;
 
         private:
+            /* Max size of send and receive packet queues for connections */
+            unsigned const packet_queue_capacity;
+
             boost::asio::io_context io_context; /* This is thread safe */
             std::jthread io_context_thread;
-
-            boost::asio::ip::tcp::acceptor acceptor;
-
-            /* These sockets should be synchronously moved into connections */
-            std::vector<boost::asio::ip::tcp::socket> accepted_sockets;
-            mutable std::mutex accepted_sockets_mx;
-
-            std::unordered_map<Connection::Id, Connection> connections;
-            Connection::Id next_connection_id;
-
-            unsigned const packet_queue_capacity;
-            std::vector<std::unique_ptr<Protocol::Inbound_packet>> inbound_packets_queue;
-            mutable std::mutex inbound_packets_queue_mx;
-            std::vector<std::shared_ptr<Protocol::Outbound_packet>> outbound_packets_queue;
-            mutable std::mutex outbound_packets_queue_mx;
-
             void io_context_thread_function(std::stop_token _stop_token);
 
+            boost::asio::ip::tcp::acceptor acceptor;
+            /* Sockets to be synchronously moved into connections */
+            std::vector<boost::asio::ip::tcp::socket> accepted_sockets;
+            mutable std::mutex accepted_sockets_mx;
+            void async_accept();
             void async_accept_callback(
                 boost::system::error_code const& _error,
                 boost::asio::ip::tcp::socket _peer_socket
             );
+            /* Move accepted_sockets into connections. This also initiates receiving packets */
+            void process_accepted_sockets();
 
+            std::vector<Events::Connection_killed> connection_killed_events;
+            mutable std::mutex connection_killed_events_mx;
+            void process_killed_connections();
+
+            std::vector<Events::Packet_received> packet_received_events;
+            mutable std::mutex packet_received_events_mx;
+            void process_received_packets();
             enum struct Async_read_operation_type { length, rest };
+            /* This does not allocate anything, length must be valid before the call */
+            void async_read(std::shared_ptr<Connection> const& _connection, Async_read_operation_type _operation);
             void async_read_callback(
-                Connection::Id _connection_id,
-                Async_read_operation_type _operation_type,
+                std::shared_ptr<Connection> _connection, /* copy */
+                Async_read_operation_type _operation, /* the operation that finished with this callback */
                 boost::system::error_code const& _error,
                 std::size_t _bytes_transferred
             );
 
+            std::unordered_map<Connection::Id, std::shared_ptr<Connection>> connections;
+            Connection::Id next_connection_id;
         };
     }
 }
